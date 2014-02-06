4edde20a0c3b2ef829eb2ecbbd55b873c8c66ba4
Khanh Nguyen <khanh@intersect.org.au>
2014-02-07 00:54:09 +1100

    Add click event handler for character

--- js/marvel_service.js
+++ js/marvel_service.js
@@ -1,5 +1,5 @@
 var publicKey = "INSERT PUBLIC KEY HERE PLZ";
-var privateKey = "DO NOT SHARE THIS WITH ANYMORE PLZ";
+var privateKey = "I REALLY NEED A BETTER WAY TO DO THIS...";
 
 var CHARACTERS = []
 var NUM_OF_CHARACTERS = 1400
@@ -43,26 +43,41 @@ function getCharacters(limit, offset) {
 
 }
 
-function parseThumbnailToHTML(url, variant, extension, charName) {
-	var img = '<a data-toggle="tooltip" title="'+charName+'"><img src="'+ url + '/' + variant + '.' +  extension + '"class="img-thumbnail char-img"/></a>';
+// Parse the the image to html with tooltip
+function parseThumbnailToHTML(url, variant, extension, charName, id) {
+	var img = '<a data-toggle="tooltip" title="'+charName+ '"' + 'id="' + id +'"><img src="'+ url + '/' + variant + '.' +  extension + '"class="img-thumbnail char-img"/></a>';
 	return img;
 }
 
-$(document).ready(function() {
+// find the relationships of a character based on the events, series and stories matching
+// GREEN LINE means 3 commons
+// ORANGE LINE means 2 commons
+// GREY LINE means 1 common - minor connection
+function findRelationships(character){
+	alert(character.name)
+}
 
+$(document).ready(function() {
+	// set offset to 100 when testing to save api calls
 	for (var offset = 0; offset <= NUM_OF_CHARACTERS; offset += 100) {
 		getCharacters(100, offset);
 	};
 
 	CHARACTERS.forEach(function(character){
 		console.log(character);
-		var img = parseThumbnailToHTML(character.thumbnail.path, PORTRAIT_SMALL, character.thumbnail.extension, character.name);
+		var img = parseThumbnailToHTML(character.thumbnail.path, PORTRAIT_SMALL, character.thumbnail.extension, character.name, character.id);
+
+		//display character thumbnails on the page
 		$(".characters").append(img);
+
+		//activate the tooltip to show character names
 		$('[data-toggle="tooltip"]').tooltip({
     		'placement': 'bottom'
 		});
-	})
 
-	//TODO: when you click on a character thumbnail - find all relationships and highlight and link
-
+		//TODO: when you click on a character thumbnail - find all relationships and highlight and link
+		$("#"+character.id).click(function(event){
+			findRelationships(character);
+		});
+	});
 });
